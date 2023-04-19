ce568db6915454fee434cb48dea837304e200b06
sahinmehmet3777 <sahinmehmet377@gmail.com>
2023-04-19 14:14:37 +0300

    Proje dosyası ekle.

---
+++ struct2812Form/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace struct2812Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.locationtxt = new System.Windows.Forms.TextBox();
+            this.agetxt = new System.Windows.Forms.TextBox();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim:";
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(147, 34);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(100, 20);
+            this.nametxt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lokasyon:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Yaş:";
+            // 
+            // locationtxt
+            // 
+            this.locationtxt.Location = new System.Drawing.Point(147, 84);
+            this.locationtxt.Name = "locationtxt";
+            this.locationtxt.Size = new System.Drawing.Size(100, 20);
+            this.locationtxt.TabIndex = 2;
+            // 
+            // agetxt
+            // 
+            this.agetxt.Location = new System.Drawing.Point(147, 139);
+            this.agetxt.Name = "agetxt";
+            this.agetxt.Size = new System.Drawing.Size(100, 20);
+            this.agetxt.TabIndex = 3;
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(162, 187);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(75, 23);
+            this.savebtn.TabIndex = 4;
+            this.savebtn.Text = "Ekle";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(32, 282);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(360, 147);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 463);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.agetxt);
+            this.Controls.Add(this.locationtxt);
+            this.Controls.Add(this.nametxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox locationtxt;
+        private System.Windows.Forms.TextBox agetxt;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ struct2812Form/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace struct2812Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            using (User u = new User(nametxt.Text, locationtxt.Text, int.Parse(agetxt.Text)))
+            {
+                listBox1.Items.Add(u.name + "-" + u.location + "-" + u.age);
+            }
+            User u1;
+            u1.name = nametxt.Text;
+            u1.location = locationtxt.Text;
+            u1.age = int.Parse(agetxt.Text);
+            listBox1.Items.Add("-----------------------------------------");
+            listBox1.Items.Add(u1.name + "-" + u1.location + "-" + u1.age);
+        }
+    }
+    struct User: IDisposable
+    {
+        public string name;
+        public string location;
+        public int age;
+        public User(string n, string a,  int b)
+        {
+            name = n;
+            location = a;
+            age = b;
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+}
